666db3a5b09ebeea8fb36e6780efa883e955283f
farhanur rahman <farhanrahman0027@gmail.com>
2025-11-01 23:25:47 +0530

    update the home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,10 +5,17 @@ const VisitorCounter = () => {
   const [visits, setVisits] = useState(0);
 
   useEffect(() => {
-    axios.post("http://localhost:5000/api/visit")
-      .then((res) => setVisits(res.data.count))
-      .catch((err) => console.error(err));
-  }, []);
+  const fetchVisits = async () => {
+    try {
+      const res = await axios.post("https://devfarhan.vercel.app/");
+      setVisits(res.data.count);
+    } catch (error) {
+      console.error("Error fetching visit count:", error);
+    }
+  };
+  fetchVisits();
+}, []);
+
 
   return (
     <div className="fixed bottom-4 right-4 bg-gray-900 text-white px-4 py-2 rounded-lg shadow-lg text-sm">
