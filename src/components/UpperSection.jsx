5c317b87f68ea946dc2f873b2925e5a919aff1ff
farhanur rahman <farhanrahman0027@gmail.com>
2026-01-02 02:07:55 +0530

    update the profile img

--- src/components/UpperSection.jsx
+++ src/components/UpperSection.jsx
@@ -17,7 +17,7 @@ import {
 } from "react-icons/fa";
 import { SiLeetcode, SiTailwindcss, SiNextdotjs, SiMongodb } from "react-icons/si";
 import { motion } from "framer-motion";
-import profile from "../assets/farhan img.jpeg";
+import profile from "../assets/img.png";
 import resume from "../assets/Resume.pdf";
 import { useDarkMode } from "../DarkModeContext";
 
